3067d7cad6e8d690955b8fe0e995637cd8d70569
Aakarshak Sthapak <akku_sthapak@live.com>
2024-05-26 17:45:22 +0530

    chore: custom Error and not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
+
+export default function NotFound() {
+  return (
+    <main className='flex h-screen items-center justify-center'>
+      <h1 className='text-4xl font-bold'>Error 404 </h1>
+      <div className='ml-2'>| This page was not found</div>
+      <Link href={'/'}>
+        <Button variant='link' className='mr-1'>
+          Return home{' '}
+        </Button>
+      </Link>
+    </main>
+  );
+}
