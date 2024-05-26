b700ead1ce54d75cc3f4e25b67136997b06f0aae
Aakarshak Sthapak <akku_sthapak@live.com>
2024-05-26 12:37:58 +0530

    feat: Init app

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next';
+import { Inter as FontSans } from 'next/font/google';
+import { cn } from '@/lib/utils';
+
+import '@/styles/globals.css';
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
+
+export const metadata: Metadata = {
+  title: 'Task It',
+  description: 'To Do it? taskIt',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>{children}</body>
+    </html>
+  );
+}
